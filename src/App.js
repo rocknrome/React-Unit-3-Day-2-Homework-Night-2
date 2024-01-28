eb83139fdad5d4c234a6d286c6e3403658f7cb85
Roman Larionov <pcsaveddotcom@gmail.com>
2024-01-28 14:19:18 -0500

    image links updated

--- src/App.js
+++ src/App.js
@@ -10,31 +10,31 @@ function App() {
       name: "Swordfish",
       year: "2001",
       rating: "R",
-      image: "https://www.example.com/swordfish.png" 
+      image: "https://m.media-amazon.com/images/I/61NJ9l5zr3L._AC_UF894,1000_QL80_.jpg" 
     },
     {
       name: "Hobbs and Shaw",
       year: "2019",
       rating: "PG-13",
-      image: "https://www.example.com/hobbsandshaw.png" 
+      image: "https://m.media-amazon.com/images/I/71T8yuUfM3L._AC_UF894,1000_QL80_.jpg" 
     },
     {
       name: "John Wick 4",
       year: "2022",
       rating: "R",
-      image: "https://www.example.com/johnwick4.png" 
+      image: "https://m.media-amazon.com/images/I/91NpGSlvlKL._AC_UF894,1000_QL80_.jpg" 
     },
     {
       name: "Split Second",
       year: "1992",
       rating: "R",
-      image: "https://www.example.com/splitsecond.png" 
+      image: "https://m.media-amazon.com/images/I/81Hyr3ICifL._AC_UF894,1000_QL80_.jpg" 
     },
     {
       name: "Matrix",
       year: "1999",
       rating: "R",
-      image: "https://www.example.com/matrix.png" 
+      image: "https://m.media-amazon.com/images/I/51DUmDryAvL._AC_UF894,1000_QL80_.jpg" 
     }
   ]);
 
